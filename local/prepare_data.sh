9815aaf059027a8a436674dddbad5a37e30e522a
JinchengHeRyan <delteforce@outlook.com>
2019-11-26 11:10:32 +0800

    Add modification on creating words.txt and file structure

--- local/prepare_data.sh
+++ local/prepare_data.sh
@@ -1,4 +1,8 @@
+mkdir data/
+mkdir data/test/
+mkdir data/train/
 # Prepare utt2spk in train
+
 find waves_data/train/ -name "*.WAV" | sort | awk -F '[/.]' '{print $5, $4}' > data/train/utt2spk
 # Prepare wav.scp in train
 find waves_data/train/ -name "*.WAV" | sort | awk -F '[/.]' '{print $5, $0}' > data/train/wav.scp
@@ -7,17 +11,27 @@ find waves_data/train/ -name "*.WAV" | sort | awk -F '[/.]' '{print $5, $0}' > d
 rm -f data/train/text
 touch data/train/text
 cd data/train
-python3 train_data_preparation.py
+python3 ../../local/train_data_preparation.py
+cd ../../
+
+# Prepare words.txt in train
+cd data/train/
+cut -d ' ' -f 2- text | sed 's/ /\n/g' | sort -u > words.txt
 cd ../../
 
 # Prepare utt2spk in test
 find waves_data/test/ -name "*.WAV" | sort | awk -F '[/.]' '{print $5, $4}' > data/test/utt2spk
-# Prepare wav.scp in train
+# Prepare wav.scp in test
 find waves_data/test/ -name "*.WAV" | sort | awk -F '[/.]' '{print $5, $0}' > data/test/wav.scp
 
-# Prepare text in train
+# Prepare text in test
 rm -f data/test/text
 touch data/test/text
 cd data/test
-python3 test_data_preparation.py
+python3 ../../local/test_data_preparation.py
+cd ../../
+
+# Prepare words.txt in test
+cd data/test/
+cut -d ' ' -f 2- text | sed 's/ /\n/g' | sort -u > words.txt
 cd ../../
