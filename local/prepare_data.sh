e5b09292e1538b5a5858aa0a8aabc877e16cae82
JinchengHeRyan <delteforce@outlook.com>
2019-11-26 19:14:00 +0800

    Change code structure of prepare_data.sh

--- local/prepare_data.sh
+++ local/prepare_data.sh
@@ -1,28 +1,22 @@
 mkdir data/
 mkdir data/test/
 mkdir data/train/
-
+waves_data_PATH=waves_data
 # Prepare utt2spk in train and test
 for x in train test; do
-    find waves_data/$x/ -name "*.WAV" | sort | awk -F '[/.]' '{print $5, $4}' > data/$x/utt2spk
-done
+    find ${waves_data_PATH}/$x/ -name "*.WAV" | sort | awk -F '[/.]' '{print $5, $4}' > data/$x/utt2spk
 
 # Prepare wav.scp in train and test
-for x in train test; do
-    find waves_data/$x/ -name "*.WAV" | sort | awk -F '[/.]' '{print $5, $0}' > data/$x/wav.scp
-done
+    find ${waves_data_PATH}/$x/ -name "*.WAV" | sort | awk -F '[/.]' '{print $5, $0}' > data/$x/wav.scp
 
 # Prepare text in train and test
-for x in train test; do
     rm -f data/$x/text
     touch data/$x/text
     cd data/$x
     python3 ../../local/data_preparation.py
     cd ../../
-done
 
 # Prepare words.txt in train and test
-for x in train test; do
     cd data/$x/
     cut -d ' ' -f 2- text | sed 's/ /\n/g' | sort -u > words.txt
     cd ../../
