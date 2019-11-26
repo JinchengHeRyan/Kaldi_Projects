c491aa49a868e792a67595f11df51ae82ef12e7c
JinchengHeRyan <delteforce@outlook.com>
2019-11-26 14:22:57 +0800

    Modification on prepare_data.sh

--- local/prepare_data.sh
+++ local/prepare_data.sh
@@ -1,37 +1,29 @@
 mkdir data/
 mkdir data/test/
 mkdir data/train/
-# Prepare utt2spk in train
 
-find waves_data/train/ -name "*.WAV" | sort | awk -F '[/.]' '{print $5, $4}' > data/train/utt2spk
-# Prepare wav.scp in train
-find waves_data/train/ -name "*.WAV" | sort | awk -F '[/.]' '{print $5, $0}' > data/train/wav.scp
+# Prepare utt2spk in train and test
+for x in train test; do
+    find waves_data/$x/ -name "*.WAV" | sort | awk -F '[/.]' '{print $5, $4}' > data/$x/utt2spk
+done
 
-# Prepare text in train
-rm -f data/train/text
-touch data/train/text
-cd data/train
-python3 ../../local/train_data_preparation.py
-cd ../../
+# Prepare wav.scp in train and test
+for x in train test; do
+    find waves_data/$x/ -name "*.WAV" | sort | awk -F '[/.]' '{print $5, $0}' > data/$x/wav.scp
+done
 
-# Prepare words.txt in train
-cd data/train/
-cut -d ' ' -f 2- text | sed 's/ /\n/g' | sort -u > words.txt
-cd ../../
+# Prepare text in train and test
+for x in train test; do
+    rm -f data/$x/text
+    touch data/$x/text
+    cd data/$x
+    python3 ../../local/data_preparation.py
+    cd ../../
+done
 
-# Prepare utt2spk in test
-find waves_data/test/ -name "*.WAV" | sort | awk -F '[/.]' '{print $5, $4}' > data/test/utt2spk
-# Prepare wav.scp in test
-find waves_data/test/ -name "*.WAV" | sort | awk -F '[/.]' '{print $5, $0}' > data/test/wav.scp
-
-# Prepare text in test
-rm -f data/test/text
-touch data/test/text
-cd data/test
-python3 ../../local/test_data_preparation.py
-cd ../../
-
-# Prepare words.txt in test
-cd data/test/
-cut -d ' ' -f 2- text | sed 's/ /\n/g' | sort -u > words.txt
-cd ../../
+# Prepare words.txt in train and test
+for x in train test; do
+    cd data/$x/
+    cut -d ' ' -f 2- text | sed 's/ /\n/g' | sort -u > words.txt
+    cd ../../
+done
