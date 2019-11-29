23e34c2939dc875a73a1e5b3b30dbba5ddb44f57
JinchengHeRyan <delteforce@outlook.com>
2019-11-29 23:24:03 +0800

    Add training and aligning code but with error

--- run.sh
+++ run.sh
@@ -11,3 +11,33 @@ for x in train test; do
  steps/make_mfcc.sh --cmd $train_cmd --nj 16 data/$x exp/make_mfcc/$x mfcc
  steps/compute_cmvn_stats.sh data/$x exp/make_mfcc/$x mfcc
 done
+
+# Monophone training and alignment
+
+# # Train
+utils/subset_data_dir.sh --first data/train 1000 data/train_1k
+
+# steps/train_mono.sh --boost-silence 1.25 --nj 10 --cmd "$train_cmd" \
+# data/train_1k data/lang exp/mono_1k
+
+# # Align
+# steps/align_si.sh --boost-silence 1.25 --nj 16 --cmd "$train_cmd" \
+# data/train data/lang exp/mono_1k exp/mono_ali || exit 1;
+
+steps/train_mono.sh  --nj 4 --cmd "$train_cmd" \
+  data/train_1k data/lang exp/mono0a
+
+ utils/mkgraph.sh data/lang exp/mono0a exp/mono0a/graph && \
+ steps/decode.sh --nj 10 --cmd "$decode_cmd" \
+      exp/mono0a/graph data/test exp/mono0a/decode
+
+steps/align_si.sh --nj 4 --cmd "$train_cmd" \
+   data/train data/lang exp/mono0a exp/mono0a_ali
+
+steps/train_deltas.sh --cmd "$train_cmd" \
+    300 3000 data/train data/lang exp/mono0a_ali exp/tri1
+
+
+ utils/mkgraph.sh data/lang exp/tri1 exp/tri1/graph
+ steps/decode.sh --nj 10 --cmd "$decode_cmd" \
+      exp/tri1/graph data/test exp/tri1/decode
