279524379ac2095171afaf2ad07e5dc4def0bfb7
JinchengHeRyan <delteforce@outlook.com>
2019-11-30 00:56:43 +0800

    Problems with prepare_lm.sh, should be fixed

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 local/prepare_data.sh waves_data
 local/prepare_dict.sh
 local/prepare_lm.sh
+utils/validate_lang.pl data/lang/
 
 # utils/prepare_lang.sh --position-dependent-phones false data/local/dict "<SIL>" data/local/lang data/lang
 
@@ -14,8 +15,9 @@ done
 
 # Monophone training and alignment
 
-# # Train
-utils/subset_data_dir.sh --first data/train 1000 data/train_1k
+# Train
+# # Subsetting data
+# utils/subset_data_dir.sh --first data/train 1000 data/train_1k
 
 # steps/train_mono.sh --boost-silence 1.25 --nj 10 --cmd "$train_cmd" \
 # data/train_1k data/lang exp/mono_1k
@@ -25,19 +27,24 @@ utils/subset_data_dir.sh --first data/train 1000 data/train_1k
 # data/train data/lang exp/mono_1k exp/mono_ali || exit 1;
 
 steps/train_mono.sh  --nj 4 --cmd "$train_cmd" \
-  data/train_1k data/lang exp/mono0a
+  data/train data/lang exp/mono0a
 
- utils/mkgraph.sh data/lang exp/mono0a exp/mono0a/graph && \
- steps/decode.sh --nj 10 --cmd "$decode_cmd" \
-      exp/mono0a/graph data/test exp/mono0a/decode
+utils/mkgraph.sh data/lang exp/mono0a exp/mono0a/graph && \
+steps/decode.sh --nj 10 --cmd "$decode_cmd" \
+   exp/mono0a/graph data/test exp/mono0a/decode
 
-steps/align_si.sh --nj 4 --cmd "$train_cmd" \
-   data/train data/lang exp/mono0a exp/mono0a_ali
-
-steps/train_deltas.sh --cmd "$train_cmd" \
-    300 3000 data/train data/lang exp/mono0a_ali exp/tri1
+steps/align_si.sh --boost-silence 1.25 --nj 16 --cmd "$train_cmd" \
+data/train data/lang exp/mono0a exp/mono0a_ali
 
 
- utils/mkgraph.sh data/lang exp/tri1 exp/tri1/graph
- steps/decode.sh --nj 10 --cmd "$decode_cmd" \
-      exp/tri1/graph data/test exp/tri1/decode
+
+# steps/align_si.sh --nj 4 --cmd "$train_cmd" \
+#    data/train data/lang exp/mono0a exp/mono0a_ali
+
+# steps/train_deltas.sh --cmd "$train_cmd" \
+#     300 3000 data/train data/lang exp/mono0a_ali exp/tri1
+
+
+#  utils/mkgraph.sh data/lang exp/tri1 exp/tri1/graph
+#  steps/decode.sh --nj 10 --cmd "$decode_cmd" \
+#       exp/tri1/graph data/test exp/tri1/decode
