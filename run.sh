5122922c1075a526503e392e9df891c344a0a485
JinchengHeRyan <delteforce@outlook.com>
2019-12-01 22:05:12 +0800

    Change run.sh and see the results

--- run.sh
+++ run.sh
@@ -33,18 +33,23 @@ utils/mkgraph.sh data/lang exp/mono0a exp/mono0a/graph && \
 steps/decode.sh --nj 10 --cmd "$decode_cmd" \
    exp/mono0a/graph data/test exp/mono0a/decode
 
-steps/align_si.sh --boost-silence 1.25 --nj 16 --cmd "$train_cmd" \
-data/train data/lang exp/mono0a exp/mono0a_ali
+# steps/align_si.sh --boost-silence 1.25 --nj 16 --cmd "$train_cmd" \
+# data/train data/lang exp/mono0a exp/mono0a_ali
+
+steps/align_si.sh --nj 4 --cmd "$train_cmd" \
+   data/train data/lang exp/mono0a exp/mono0a_ali
+
+steps/train_deltas.sh --cmd "$train_cmd" \
+    300 3000 data/train data/lang exp/mono0a_ali exp/tri1
 
 
+ utils/mkgraph.sh data/lang exp/tri1 exp/tri1/graph
+ steps/decode.sh --nj 10 --cmd "$decode_cmd" \
+      exp/tri1/graph data/test exp/tri1/decode
 
-# steps/align_si.sh --nj 4 --cmd "$train_cmd" \
-#    data/train data/lang exp/mono0a exp/mono0a_ali
-
-# steps/train_deltas.sh --cmd "$train_cmd" \
-#     300 3000 data/train data/lang exp/mono0a_ali exp/tri1
+# Example of looking at the output.
+utils/int2sym.pl -f 2- data/lang/words.txt  exp/tri1/decode/scoring/19.tra | sed "s/ $//" | sort | diff - data/test/text
 
 
-#  utils/mkgraph.sh data/lang exp/tri1 exp/tri1/graph
-#  steps/decode.sh --nj 10 --cmd "$decode_cmd" \
-#       exp/tri1/graph data/test exp/tri1/decode
+# Getting results [see RESULTS file]
+for x in exp/*/decode*; do [ -d $x ] && grep SER $x/wer_* | utils/best_wer.sh; done
